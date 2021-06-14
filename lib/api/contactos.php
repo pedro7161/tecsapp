c468c08297b3ded54da0f3c5533c57d0e26d14e4
pedro7161 <pedro.ramos9@hotmail.com>
2021-06-14 17:21:55 +0100

    funçao add contact logica sql

--- lib/api/contactos.php
+++ lib/api/contactos.php
@@ -34,12 +34,52 @@ function addContact($user)
         "SELECT ID from `cliente` where nome=?"
     );
 
+
     $query2 = $db->prepare(
-        "INSERT INTO  `contactos` () VALUES() "
+        "INSERT INTO  `contactos` (ID_Client,ID_Friend) VALUES(?,?) "
     );
+    $cheat1 = "type";
+    $cheat2 = "date";
+    $body = "Hi!!";
+
     $query3 = $db->prepare(
+        "INSERT INTO  `mensagens` (body,ler, $cheat1,id_sender,id_receiver,$cheat2) VALUES($body,'0','enviado',?,?,datetime.now) "
+    );
+    $query4 = $db->prepare(
         "SELECT ID from `cliente` where nome=?"
     );
+
+    $query->bind_param("s", $user);
+    $result = $query->execute();
+    if (!$result) {
+        echo "ERROR: Trying to verify user";
+        return;
+    }
+    $user = $query->get_result();
+    if (!$user) {
+
+        echo "ERROR: Trying to get result from db";
+        return;
+    }
+    $user = $user->fetch_assoc();
+    $query4->bind_param("s", $onlinne);
+    $result = $query->execute();
+    if (!$result) {
+        echo "ERROR: Trying to verify user";
+        return;
+    }
+    $online = $query->get_result();
+    if (!$online) {
+
+        echo "ERROR: Trying to get result from db";
+        return;
+    }
+    $online = $online->fetch_assoc();
+
+
+
+    $query2->bind_param("ss", $online["ID"], $user["ID"]);
+    $query3->bind_param("ss", $online["ID"], $user["ID"]);
 }
 
 
